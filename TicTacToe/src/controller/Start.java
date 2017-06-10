d91e73518ca981b2cf51738c98f7c676822411d5
Kumar72 <vichandan@gmail.com>
2017-06-10 16:36:06 -0400

    Home screen for 2 player or player vs ai

--- TicTacToe/src/controller/Start.java
+++ TicTacToe/src/controller/Start.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.Scanner;
 
+import data.Player;
 import data.Screen;
 
 public class Start {
@@ -11,5 +12,27 @@ public class Start {
 	Screen screen = new Screen();
 	screen.homeScreen(0);
 	screen.homeScreen(kb.nextInt());
+	}
+	
+	public Player playerInfo(int i) {
+		Player player = new Player();
+		String x = "";
+		if(i == 0) {
+			x = "Player Name: ";
+		}
+		else if(i == 1) {
+			x = "Player1 Name: ";
+		}
+		else if(i == 2) {
+			x = "PLayer2 Name: ";
+		}
+		else{
+//			Throw an error prompt here
+		}
+		System.out.print(x);
+		player.setName(kb.next());
+		System.out.print("Player Symbol: ");
+		player.setSymbol(kb.next().charAt(0));
+		return player;
 	}
 }
--- TicTacToe/src/data/Screen.java
+++ TicTacToe/src/data/Screen.java
@@ -1,18 +1,39 @@
 package data;
 
+import controller.Start;
+
 public class Screen {
-	Player player = new Player();
+	
+	Start start = new Start();
 	public void homeScreen(int i) {
-		String[] line = {" "," "," "," "," "," "," "};
+		String[] line = {" "," "," "," "," "," "," "," "};
 		
 		switch(i) {
 		case 0:
 			line[0] = "Welcome to TicTacToe";
-			line[4] = "1. Player vs AI";
-			line[5] = "2. Player vs Player";
+			line[4] = "[1] Player vs AI";
+			line[5] = "[2] Player vs Player";
 			break;
 		case 1:
-			line[1] = "Player Set up";
+			line[0] = "Player VS AI";
+			Player player = start.playerInfo(0);
+			line[2] = "Player: \t"+player.getName();
+			line[3] = "Symbol: \t"+player.getSymbol();
+//			get player info here and display it
+//			when player info is filled prompt user to play the game
+			line[7] = "[0] Exit";
+			break;
+		case 2:
+			line[0] = "Player VS Player";
+			Player p1 = start.playerInfo(1);
+			line[2] = "Player: \t"+p1.getName();
+			line[3] = "Symbol: \t"+p1.getSymbol();
+			Player p2 = start.playerInfo(2);
+			line[4] = "Player: \t"+p2.getName();
+			line[5] = "Symbol: \t"+p2.getSymbol();
+//			get both players and display there info
+//			
+			line[7] = "[0] Exit";
 			break;
 		}
 		
@@ -25,9 +46,15 @@ public class Screen {
 		System.out.println("             "+line[4]+"               ");		
 		System.out.println("             "+line[5]+"               ");		
 		System.out.println("             "+line[6]+"               ");		
+		System.out.println("             "+line[7]+"               ");		
 		System.out.println("|                                              |");
 		System.out.println("------------------------------------------------");
 		
+	}
+	
+	public Player playerInfo() {
+		Player p1 = new Player();
+		return null;
 	}
 	
 	public void gameScreen(Player p1, Player p2) {
@@ -35,6 +62,7 @@ public class Screen {
 		System.out.println("------------------------------------------------");
 		System.out.println("|                                              |");
 		System.out.println("|  "+p1.getName()+"["+p1.getSymbol()+"]                                     "+p2.getName()+"["+p2.getSymbol()+"]   |");
+		System.out.println("|                                              |");
 		System.out.println("|                                              |");
 		System.out.println("|                  "+c[0]+" | "+c[1]+"  | "+c[2]+"                     |");
 		System.out.println("|               --------------                 |");
